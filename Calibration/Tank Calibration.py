44777753a7b57c8927946aec91e7fe4b8b0d0fe5
Riya Aggarwal <2000riya@gmail.com>
2016-01-27 01:41:51 -0600

    Tank Game Calibration Adjustment
    Adjusted the sizing and position of the squares to help the vision

---
+++ Calibration/Tank Calibration.py
@@ -0,0 +1,47 @@
+import pygame
+import time
+
+
+pygame.init()
+
+blue = (73, 109, 173)
+white = (255, 255, 255)
+
+gameExit = False
+clock = pygame.time.Clock()
+
+# Set the height and width of the display
+displaywidth = 600
+displayheight = 400
+
+gameDisplay = pygame.display.set_mode([displaywidth, displayheight],pygame.FULLSCREEN)
+pygame.display.set_caption('ARbattles Calibrations')
+
+
+while not gameExit:
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+    if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                gameExit = True
+                
+    gameDisplay.fill(white)
+    pygame.draw.rect(gameDisplay, blue ,[0,0,displaywidth/2,displayheight/2])
+    pygame.draw.rect(gameDisplay, blue ,[displaywidth/2,displayheight/2,displaywidth/2,displayheight/2])
+    
+    """
+    pygame.draw.rect(gameDisplay, blue ,[0,0,50,50])
+    pygame.draw.rect(gameDisplay, blue ,[displaywidth - 50, displayheight - 50, 50, 50])
+    pygame.draw.rect(gameDisplay, blue ,[displaywidth - 50,0,50,50])
+    pygame.draw.rect(gameDisplay, blue ,[0,displayheight - 50,50,50])
+    """
+    pygame.display.update()
+  
+    clock.tick(20)
+    
+pygame.quit()
+quit()
+
+
+
